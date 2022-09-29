f6cdb5c97d08762804dc87d06a048463c7c3aab0
Jhonatan Rico <xricoxdrumsx@gmail.com>
2022-09-29 17:51:03 -0500

    Database connection configurations

--- src/main/java/com/ufriend/UFriendApplication.java
+++ src/main/java/com/ufriend/UFriendApplication.java
@@ -1,13 +1,32 @@
 package com.ufriend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class UFriendApplication {
+public class UFriendApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UFriendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "CREATE TABLE TEST_TABLE(" +
+				"id INT PRIMARY KEY," +
+				"name VARCHAR(30));" +
+				"" +
+				"INSERT INTO TEST_TABLE(id, name) VALUES (1, 'Test Value')";
+
+		int rows = jdbcTemplate.update(sql);
+		if (rows > 0) {
+			System.out.println("A new row has been inserted.");
+		}
+	}
 }
