c6f25a6be63ec396a5e8b5956cb5e2c97e9bc1fa
Yesid Gonzalez <oquintero@openmytab.com>
2022-10-02 17:27:41 -0500

    set configuration cors

--- src/main/java/com/ufriend/controllers/authController.java
+++ src/main/java/com/ufriend/controllers/authController.java
@@ -12,20 +12,7 @@ import com.ufriend.user.UserEntity;
 import com.ufriend.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class authController {
