e2e1130625d52fed050722efcea1def77adca0f4
Alejo <alejito23001@gmail.com>
2022-10-11 10:12:32 -0500

    NEW: Enum generated for reference a template

---
+++ src/main/java/com/ufriend/enums/EmailTemplate.java
@@ -0,0 +1,39 @@
+package com.ufriend.enums;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+public enum EmailTemplate {
+    
+    CONFIRMATION(getHtmlFile("confirmation")),
+    ACCOUNT_DELETED(getHtmlFile("account_deleted")),
+    RESTORE_PASSWORD(getHtmlFile("restore_password"));
+
+    private String content;
+
+    private EmailTemplate (String content) {
+        this.content = content;
+    }
+
+    private static String getHtmlFile(String fileName) {
+        fileName = String.format("src/main/resources/templates/email/%s.html", fileName);
+        StringBuilder contentBuilder = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            String str;
+            while ((str = in.readLine()) != null) {
+                contentBuilder.append(str);
+            }
+            in.close();
+        } catch (IOException e) {
+        }
+        String content = contentBuilder.toString();
+        return content;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
