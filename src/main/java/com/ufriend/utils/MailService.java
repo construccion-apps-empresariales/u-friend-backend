b0b4397edaf72dd0bdda91909fc875c61351572d
Alejo <alejito23001@gmail.com>
2022-10-13 17:08:42 -0500

    UPDATE: Mail sender adapted to support html files as email's body

--- src/main/java/com/ufriend/utils/MailService.java
+++ src/main/java/com/ufriend/utils/MailService.java
@@ -1,17 +1,21 @@
 package com.ufriend.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.ufriend.enums.EmailTemplate;
+
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @Data
+@Slf4j
 public class MailService {
 
     @Autowired
@@ -20,25 +24,40 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String from;
 
-    public Boolean send(String to, String subject, String body) {
-
-        Boolean sended;
-
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(from);
-            message.setTo(to);
-            message.setText(body);
-            message.setSubject(subject);
-    
-            mailSender.send(message);
-            sended = true;
-        } 
-        catch(Exception e){
-            log.error("Error enviando correo: " + e);
-            sended = false;
-        }
-
-        return sended;
+    public Boolean sendConfirmationEmail(String to, String token) {
+        return send(EmailTemplate.CONFIRMATION, to, token);
     }
+    
+    public Boolean sendRestorePasswordEmail(String to, String token) {
+        return send(EmailTemplate.RESTORE_PASSWORD, to, token);
+    }
+
+    public Boolean sendAccountDeletedEmail(String to) {
+        String token = "";
+        return send(EmailTemplate.ACCOUNT_DELETED, to, token);
+    }
+
+    private Boolean send(EmailTemplate template, String to, String token) {
+        MimeMessagePreparator message = mimeMessage -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            helper.setFrom(this.from);
+            helper.setTo(to);
+            helper.setSubject(template.getSubject());
+            String content = template.getContent().
+                replace("TOKEN", token);
+            log.info(content);
+            helper.setText(content, true);
+        };
+        
+        try {
+            this.mailSender.send(message);
+            log.info(String.format("Email sended to %s with template %s", to, template.getSubject()));
+            return true;
+        }
+        catch (Exception e) {
+            log.error(String.format("Error sending email to %s.\nError:%s", to, e.toString()), e);
+            return false;
+        }        
+    }
+
 }
