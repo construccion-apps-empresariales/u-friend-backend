d2e92c5f481f2824e86c5d133426b4dc3927ffa6
Alejo <alejito23001@gmail.com>
2022-10-18 02:21:00 -0500

    NEW: Get all themes method was implemented.

--- src/main/java/com/ufriend/controllers/ThemesController.java
+++ src/main/java/com/ufriend/controllers/ThemesController.java
@@ -1,10 +1,14 @@
 package com.ufriend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.ufriend.dto.ResponseDTO;
 import com.ufriend.theme.ThemeDao;
+import com.ufriend.theme.ThemeEntity;
 import com.ufriend.theme.ThemeService;
 
 @RestController()
@@ -15,5 +19,13 @@ public class ThemesController {
 
     @Autowired
     ThemeService themeService;
+
+    @GetMapping("/themes")
+    public ResponseEntity<ResponseDTO> getAll() {
+        Iterable<ThemeEntity> themes = themeDao.findAll();
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new ResponseDTO(true, null, themes));
+    }
 
 }
