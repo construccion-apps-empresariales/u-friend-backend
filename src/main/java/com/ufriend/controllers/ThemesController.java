908f167b2040a7f9257f454bfdce91b021f8f393
Alejo <alejito23001@gmail.com>
2022-10-20 01:49:16 -0500

    NEW: Create themes method implemented

--- src/main/java/com/ufriend/controllers/ThemesController.java
+++ src/main/java/com/ufriend/controllers/ThemesController.java
@@ -1,44 +1,67 @@
 package com.ufriend.controllers;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ufriend.dto.ResponseDTO;
+import com.ufriend.dto.http.ResponseDTO;
 import com.ufriend.theme.ThemeEntity;
 import com.ufriend.theme.ThemeService;
 
 @RestController()
-public class ThemesController {
+public class ThemesController extends ExceptionHandlerController {
 
     @Autowired
     ThemeService themeService;
 
+    // Get all themes
     @GetMapping("/themes")
     public ResponseEntity<ResponseDTO> getAll() {
         Iterable<ThemeEntity> themes = themeService.list();
         return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(new ResponseDTO(true, null, themes));
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(true, null, themes));
     }
 
-    @GetMapping("/theme/{id}")
+    // Get one theme
+    @GetMapping("/themes/{id}")
     public ResponseEntity<ResponseDTO> getOne(@PathVariable(name = "id") String id) {
         ThemeEntity theme = themeService.findById(id);
         if (theme == null) {
             return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ResponseDTO(true, String.format("Theme not with id '%s' found", id), null));
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseDTO(
+                            false,
+                            String.format("Theme with id '%s' not found", id),
+                            null));
         }
 
         return ResponseEntity
-            .status(HttpStatus.OK)
-            .body(new ResponseDTO(true, null, theme));
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO(true, null, theme));
     }
 
+    // TODO SetProtected
+    // Cretae a theme
+    @PostMapping("/themes")
+    public ResponseEntity<ResponseDTO> create(@Valid @RequestBody ThemeEntity body) {
+        ThemeEntity theme = this.themeService.findById(body.getId());
+        if (theme != null) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new ResponseDTO(false, "Theme alredy exist's", theme));
+        }
+        this.themeService.save(body);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDTO(true, "Theme created", null));
+    }
 
 }
